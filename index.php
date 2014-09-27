baf2cda8bf551ded6905862c9be85b3d87b8cd44
Brad Bodily <bod12002@byui.edu>
2014-09-26 18:46:55 -0600

    Upload some stuff

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Hello, World</title>
+  <link rel="stylesheet" type="text/css" href="style/bcb.css" media="screen">
+  <title>BCBodily - CS 313</title>
 </head>
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    </header>
+    <h1>20 Questions</h1>
+    <h2>H2 Level Heading</h2>
 Hello, World. Coming from GitHub now.
+<p>Test Paragraph</p>
 </body>
 </html>
