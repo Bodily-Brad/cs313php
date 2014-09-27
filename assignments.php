bffed77d001634768adba5f45e642ffae69dc06b
Brad Bodily <bod12002@byui.edu>
2014-09-26 18:57:29 -0600

    Next step

--- assignments.php
+++ assignments.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <link rel="stylesheet" type="text/css" href="style/bcb.css" media="screen">
-  <title>BCBodily - CS 313</title>
+  <title>About BCBodily - CS 313</title>
 </head>
 <body>
     <header>
@@ -16,9 +16,7 @@
             </ul>
         </nav>
     </header>
-    <h1>20 Questions</h1>
-    <h2>H2 Level Heading</h2>
-Hello, World. Coming from GitHub now.
-<p>Test Paragraph</p>
+    <h1>Assignments</h1>
+    <h2>Coming Soon</h2>
 </body>
 </html>
