e7f8b2ec3d13f0ee477c20c8c1404051c984d3ee
Brad Bodily <bod12002@byui.edu>
2014-10-03 16:03:06 -0600

    2.03 Start

---
+++ assignments/2.03/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="./style/bcb.css" media="screen">
+  <title>BCBodily - CS 313 - Assignment 2.03</title>
+</head>
+<body>
+<!-- Header -->
+<?php include './includes/index.php';  ?>
+    <h1>PHP Survey</h1>
+    <?php echo $_SERVER['DOCUMENT_ROOT'] . '/php/' ?>
+    <!-- Footer -->
+<?php include './includes/_footer.php';  ?>
+</body>
+</html>
