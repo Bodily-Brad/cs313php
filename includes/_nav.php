a55042458f0a4c9e3af56442fa6d803069002daf
Brad Bodily <bod12002@byui.edu>
2014-10-15 17:38:33 -0600

    Little change - just reordered the navigation items

--- includes/_nav.php
+++ includes/_nav.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="/" title="The Home Page">Home</a></li>
+        <li><a href="/game/" title="The Guessing Game">The Game</a></li>        
         <li><a href="/assignments.php" title="Class Assignments">Assignments</a></li>
-        <li><a href="/game/" title="The Guessing Game">The Game</a></li>
     </ul>
 </nav>
