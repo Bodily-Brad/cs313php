11aa0c82e3d3d48557606922d1a1d6a29bbbdf92
Brad Bodily <bod12002@byui.edu>
2014-10-29 10:05:56 -0600

    Removed the "management" links - so that new questions and items aren't "accidentally" added by anyone.

--- views/readOnly.php
+++ views/readOnly.php
@@ -7,15 +7,16 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/_header.php';  ?>
     <h1>Game Menu</h1>
     <a href="/game/play/">Play The Game (in progress)</a>
+    <h1>Help Teach The Game</h1>
+    <a href='/game/teach/' title='Teach the game'>Teach the game</a>    
     <h1>Read-Only Database Access</h1>
 <?php include $_SERVER["DOCUMENT_ROOT"]  . '/views/_searchItemsForm.php'; ?>
 <?php include $_SERVER["DOCUMENT_ROOT"]  . '/views/_searchQuestionsForm.php'; ?>
 <?php include $_SERVER["DOCUMENT_ROOT"]  . '/views/_searchAnswersForm.php'; ?>
-    <h1>Help Teach The Game</h1>
-    <a href='/game/teach/' title='Teach the game'>Teach the game</a>
-    <h1>Manage The Game</h1>
+
+<!--    <h1>Manage The Game</h1>
     <a href="manage/?action=ManageItems">Manage Items</a><br>
-    <a href="manage/?action=ManageQuestions">Manage Questions</a><br>    
+    <a href="manage/?action=ManageQuestions">Manage Questions</a><br>    -->
     <!-- Footer -->
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/_footer.php';  ?>    
 </body>
