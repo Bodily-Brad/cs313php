6a7a98053bbce67cae9ff92e533b72de74cc57b3
Brad Bodily <bod12002@byui.edu>
2014-10-29 12:16:30 -0600

    Gremlins in last batch... hopefully they are gone Changed teach logic to use fall through

--- game/teach/index.php
+++ game/teach/index.php
@@ -24,40 +24,27 @@
             {
                 Response::IncrementCount($itemID, $questionID, $answerID);
                 $message = "Answer recorded. Thank you.<br>";
-            }
-                       
-            // Get Random Item
-            $items = Item::LoadAllFromDatabase();
-            $itemID = mt_rand(0,count($items)-1);
-            // Get random item's ItemID
-            $itemID = $items[$itemID]->GetItemID();
-            $itemID = Response::GetItemIDWithLowestResponseCount();             
-            
-            // Get random questionID
-            $questions = Question::LoadAllFromDatabase();
-            $questionID = mt_rand(0, count($questions)-1);
-            $questionID = $questions[$questionID]->GetKey();
-            $questionID = Response::GetQuestionIDWithLowestResponseCountForItem($itemID);
-            
-            $answers = Answer::LoadAllFromDatabase();
-            include($_SERVER["DOCUMENT_ROOT"] . '/views/game/teach.php');
-            break;             
-        // Random Item - Random Question
-        case "none":
+            }           
+        // Fall through
         default:
-            // Get Random Item
             require_once $_SERVER["DOCUMENT_ROOT"]  . '/models/item.php';
             require_once $_SERVER["DOCUMENT_ROOT"]  . '/models/question.php';
             require_once $_SERVER["DOCUMENT_ROOT"]  . '/models/answer.php';
             require_once $_SERVER["DOCUMENT_ROOT"]  . '/models/response.php';
 
+            // Get Random Item
             $items = Item::LoadAllFromDatabase();
-            $itemID = mt_rand(0,count($items)-1);
+            //$itemID = mt_rand(0,count($items)-1);
             // Get random item's ItemID
-            $itemID = $items[$itemID]->GetItemID();
-            $itemID = Response::GetItemIDWithLowestResponseCount();                
+            //$itemID = $items[$itemID]->GetItemID();
+            $itemID = Response::GetItemIDWithLowestResponseCount();                 
             
+            // Get random questionID
             $questions = Question::LoadAllFromDatabase();
+            //$questionID = mt_rand(0, count($questions)-1);
+            //$questionID = $questions[$questionID]->GetKey();
+            $questionID = Response::GetQuestionIDWithLowestResponseCountForItem($itemID);
+            
             $answers = Answer::LoadAllFromDatabase();
             include($_SERVER["DOCUMENT_ROOT"] . '/views/game/teach.php');
             break;                       
