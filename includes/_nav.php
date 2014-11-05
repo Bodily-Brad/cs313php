66ec371db0056f4141f8b7b87ab703bc7970f43d
Brad Bodily <bod12002@byui.edu>
2014-11-05 11:41:18 -0700

    Added link to Java servlet

--- includes/_nav.php
+++ includes/_nav.php
@@ -3,5 +3,6 @@
         <li><a href="/" title="The Home Page">Home</a></li>
         <li><a href="/game/" title="The Guessing Game">The Game</a></li>        
         <li><a href="/assignments.php" title="Class Assignments">Assignments</a></li>
+        <li><a href="http://java-bcbodily.rhcloud.com/HelloWorld/HelloServlet" title="Java HelloWorld Servlet">Java Servlet</a></li>
     </ul>
 </nav>
