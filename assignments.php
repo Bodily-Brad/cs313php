2993c8b650b9012915bd98f35aace465b31f10d5
Brad Bodily <bod12002@byui.edu>
2014-11-18 07:35:31 -0700

    Added link for 8.02 project

--- assignments.php
+++ assignments.php
@@ -11,6 +11,7 @@
     <h1>Assignments</h1>
     <ul>
         <li><a href="assignments/2.03/">2.03 PHP Survey</a> - Answer a few questions about a few guys...</li>
+        
         <li><a href="http://java-bcbodily.rhcloud.com/8.03/login.jsp">8.03 JSP Discussion Thread</a></li>
     </ul>
     <h1>Group Assignments</h1>
@@ -18,6 +19,7 @@
         <li><a href="group/4.02/scriptures.php">4.02 - Scripture Database</a></li>
         <li><a href="group/5.02/">5.02 - Scripture Database (with Insert)</a></li>
         <li><a href="group/6.02/">6.02 - User creation/login</a></li>
+        <li><a href="http://java-bcbodily.rhcloud.com/8.02/login.jsp">8.02 - User login</a></li>
     </ul>
     <!-- Footer -->
 <?php include './includes/_footer.php';  ?>    
